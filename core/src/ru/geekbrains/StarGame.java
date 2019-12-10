1e543d2013b29a3811aca982bbb40673a0cd8523
KonstantinGlady <gladyi@hotmail.com>
2019-12-10 20:47:50 +0300

    added new texture

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -9,19 +9,22 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture imgDrama;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//img = new Texture("badlogic.jpg");
+		imgDrama = new Texture("dramaQueen.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1f,0.75f,0.79f,1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+	//	batch.draw(img, 0, 0);
+		batch.draw(imgDrama,200,100);
 		batch.end();
 	}
 	
