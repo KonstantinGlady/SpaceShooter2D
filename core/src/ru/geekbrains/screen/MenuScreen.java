e83426d99fc617ec14baa0564c26cd8cd02f512d
KonstantinGlady <gladyi@hotmail.com>
2019-12-22 04:37:14 +0300

    third inite

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -1,106 +1,73 @@
 package ru.geekbrains.screen;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.BaseScreen;
+import ru.geekbrains.base.Sprite;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.sprite.Background;
+
 
 public class MenuScreen extends BaseScreen {
 
-    //TODO Реализовать движение логотипа badlogic (можно свою картинку вставить)
-    // при нажатии клавиши мыши (touchDown) в точку нажатия на экране и остановку в данной точке.
-
     private Texture img;
-    private Texture background;
+    private Texture bg;
     private Vector2 pos;
-    private Vector2 v;
 
-    private float speedMax;
-    private float destinyY;
-    private float destinyX;
+
+    private Background background;
+
 
     @Override
     public void show() {
         super.show();
-        background = new Texture("textures/bg.png");
-        img = new Texture("dramaQueen.jpg");
-        pos = new Vector2(150, 150);
-        v = new Vector2();
+        bg = new Texture("textures/bg.png");
+        background = new Background(new TextureRegion(bg));
+        img = new Texture("badlogic.jpg");
+        pos = new Vector2();
 
-        speedMax = 2;
-        destinyX = 0;
-        destinyY = 0;
     }
 
     @Override
     public void render(float delta) {
-
         super.render(delta);
-        Gdx.gl.glClearColor(0.2f, 0.6f, 0.5f, 1);
+        Gdx.gl.glClearColor(0.2f, 	0.6f, 0.5f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.begin();
-        batch.draw(background, 0, 0);
-        batch.draw(img, pos.x, pos.y);
+        background.draw(batch);
+        batch.draw(img, pos.x, pos.y, 0.5f, 0.5f);
         batch.end();
 
-        if (pos.dst(destinyX, destinyY) > 1) {
-            pos.add(v);
-        }
 
     }
 
     @Override
     public void dispose() {
         img.dispose();
-        background.dispose();
+        bg.dispose();
         super.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        destinyX = screenX;
-        destinyY = Gdx.graphics.getHeight() - screenY;
-
-        v.set(destinyX - pos.x, destinyY - pos.y).nor().scl(speedMax);
-        return false;
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
     }
-
-    //TODO Реализовать управление логотипом с помощью клавиш
-    // со стрелками на клавиатуре*** (дополнительное задание, не обязательное к выполнению)
 
     @Override
-    public boolean keyDown(int keycode) {
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
 
-       v.set(0,0);
+       return super.touchDown(touch, pointer, button);
 
-        switch (keycode) {
-            case Keys.UP:
-
-                pos.add(0, 2);
-               break;
-
-            case Keys.DOWN:
-
-                pos.add(0, -2);
-                break;
-            case Keys.LEFT:
-
-                pos.add(-2, 0);
-                break;
-            case Keys.RIGHT:
-
-                pos.add(2, 0);
-                break;
-
-            default:
-
-        }
-        return super.keyDown(keycode);
     }
+
 
 
 }
+
+
